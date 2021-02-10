a09bafbbf5bc55da21829654238456426136e9eb
SerhiiRuban81 <ruban_sa@hotmail.com>
2021-02-11 01:36:47 +0200

    Initial commit

---
+++ DatingApp.API/Controllers/ItemsController.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly MyDbContext context;
+        public ItemsController(MyDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var items = await context.Items.ToListAsync();
+            return Ok(items);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id){
+            CartItem item = await context.Items.FirstOrDefaultAsync(x=>x.Id == id);
+            return Ok(item);
+        }
+
+    }
+}
---
+++ DatingApp.API/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options){
+
+        }
+
+        public DbSet<CartItem> Items { get; set; }
+         
+    }
+}
